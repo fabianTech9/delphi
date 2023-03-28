f0b9c37b48a0062be580215dd7869cae06371e28
Fabian Moreno <fabian.moreno@tech9.com>
2023-03-28 14:38:24 -0500

    Adding squeeze back notification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,16 @@ const Main: NextPage = ({ data }: SlugProps): JSX.Element => (
         href="/icons/squeezeback-notification.svg"
         rel="preload"
       />
+      <link
+        as="image"
+        href="https://ocmproduction-delphitheater.ocecdn.oraclecloud.com/content/published/api/v1.1/assets/CONT680EB28C349544D9B0DFD1A1B9A271B0/native/real-time-player-pause-0100.png?channelToken=d57d3338641c4004a255b7be5e742af6"
+        rel="preload"
+      />
+      <link
+        as="image"
+        href="https://ocmproduction-delphitheater.ocecdn.oraclecloud.com/content/published/api/v1.1/assets/CONTA2F19C865F9145AEA78127858F76AB79/native/Lower+Third.png?channelToken=d57d3338641c4004a255b7be5e742af6"
+        rel="preload"
+      />
       <script src="https://dl.phenixrts.com/JsSDK/2022.0.latest/min/channels.js" />
     </Head>
     <main>
